06139da7c7cccd0df124831f928b8a78094f96ff
OHM <ohm.ratchaphon@gmail.com>
2025-07-12 18:31:33 +0700

    fix number2

--- week-2/number2.c
+++ week-2/number2.c
@@ -4,43 +4,11 @@ int main() {
     char number[6];
     scanf("%s", number);
 
-    char d1 = number[0];
-    char d2 = number[1];
-    char d3 = number[2];
-    char d4 = number[3];
-    char d5 = number[4];
-
-    int i;
-
-    printf("%c", d1);
-    i = 0;
-    while (i < 80) {
-        putchar(32);
-        i++;
-    }
-
-    printf("%c%c", d1, d2);
-    i = 0;
-    while (i < 79) {
-        putchar(32);
-        i++;
-    }
-
-    printf("%c%c%c", d1, d2, d3);
-    i = 0;
-    while (i < 78) {
-        putchar(32);
-        i++;
-    }
-
-    printf("%c%c%c%c", d1, d2, d3, d4);
-    i = 0;
-    while (i < 77) {
-        putchar(32);
-        i++;
-    }
-
-    printf("%c%c%c%c%c", d1, d2, d3, d4, d5);
+    printf("%-81.1s", number);
+    printf("%-81.2s", number);
+    printf("%-81.3s", number);
+    printf("%-81.4s", number);
+    printf("%-81.5s", number);
 
     return 0;
 }
