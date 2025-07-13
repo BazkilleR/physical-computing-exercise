a3c9bff7f76c9f55456b6e3fc0df535c0b724283
OHM <ohm.ratchaphon@gmail.com>
2025-07-13 22:17:56 +0700

    fix number.c

--- week-2/number.c
+++ week-2/number.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int number;
+    char n1, n2, n3, n4, n5;
 
-    scanf("%d", &number);
-
-    int d1 = number / 10000;
-    int d2 = (number / 1000) % 10;
-    int d3 = (number / 100) % 10;
-    int d4 = (number / 10) % 10;
-    int d5 = number % 10;
-
-    printf("%d%d%d%d%d", d3, d4, d5, d1, d2);
+    scanf("%c%c%c%c%c", &n1, &n2, &n3, &n4, &n5);
+    printf("%c%c%c%c%c", n3, n4, n5, n1, n2);
 
     return 0;
 }
