ce220830c0382d8c66529cdbdf41ccb6478e85e7
OHM <ohm.ratchaphon@gmail.com>
2025-07-26 22:48:14 +0700

    add more

--- lab04/07-shirtSales.c
+++ lab04/07-shirtSales.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    float price, discount, promotion_1, promotion_2;
-    int amount;
+int main(){
+    double price;
+    int sell,n;
+    scanf("%lf", &price);
+    scanf("%d", &sell);
+    scanf("%d", &n);
 
-    scanf("%f", &price);
-    scanf("%f", &discount);
-    scanf("%d", &amount);
+    double p1 = (n*price) * (100-sell)/100;
+    double p2 = ((n/3) * price * 2) + n%3 * price ;
 
-    promotion_1 = price * amount * (discount / 100.0)
-    
-    for ()
-    
+    if (p1 <= p2){
+        printf("Discount %d%%\n", sell);
+        printf("%.2lf\n",p1);
+    }else{
+        printf("Buy 2 Get 1\n");
+        printf("%.2lf\n",p2);
+    }
+
     return 0;
 }
--- lab04/09-alcohol.c
+++ lab04/09-alcohol.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-int main() {
-    float a, b, c;
-    bool first, second, third;
+int main()
+{
+    char sex, haveB;
+    float kg, cc, blood;
+    int drink_amount, hr, n;
+    scanf(" %c", &sex);
+    scanf(" %f", &kg);
+    scanf(" %c", &haveB);
+    scanf(" %f", &cc);
+    scanf(" %d", &drink_amount);
+    scanf(" %d", &n);
+    scanf(" %d", &hr);
 
-    scanf("%f", &a);
-    scanf("%f", &b);
-    scanf("%f", &c);
+    float drinkN = ((drink_amount / 100.0) * cc) * n;
 
-    first = a + b > c;
-    second = a + c > b;
-    third = b + c > a;
+    float r = (sex == 'M') ? 0.68 : 0.55;
+    blood = (drinkN / (kg * r * 10.0)) * 1000.0 - (hr * 15.0);
 
-    if (first && second && third) {
-        printf("Triangle is valid.");
+    if (blood <= 50.0 && haveB == 'Y') {
+        printf("Safe");
     } else {
-        printf("Triangle is not valid.");
+        printf("Not Safe");
     }
-    
+
     return 0;
 }
